752bf93ac2560e6f49b5df7e528a12352d997550
Yuliia Hlushko <y.glushko@outlook.com>
2022-12-29 23:23:00 +0200

    added 2 tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const videoTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+
+const onPlay = function (data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(videoTime);
+
 
